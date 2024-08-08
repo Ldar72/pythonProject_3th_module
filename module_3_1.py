d37d920a827c67a990ab68643c190156d4b760b5
Ldar72 <l-dar@mail.ru>
2024-08-08 15:51:28 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -1,12 +1,15 @@
 calls = 0
 
+
 def count_calls():
     global calls
     calls += 1
 
+
 def string_info(string):
     count_calls()
-    return (len(string), string.upper(), string.lower())
+    return len(string), string.upper(), string.lower()
+
 
 def is_contains(string, list_to_search):
     count_calls()
@@ -16,11 +19,11 @@ def is_contains(string, list_to_search):
     return False
 
 
-print(string_info("CarMen"))
-print(is_contains("Moscow", ["London", "Paris", "Tokio"]))
-print(is_contains("яблоко", ["ананас", "яблоко", "гранат"]))
-print(string_info("Hello, Teacher!"))
-print(is_contains("TEACHER", ["master", "chef", "sir", "lord"]))
-print(string_info("Ragnarok"))
+print(string_info('CarMen'))
+print(is_contains('Moscow', ['London', 'Paris', 'Tokio']))
+print(is_contains('яблоко', ['ананас', 'яблоко', 'гранат']))
+print(string_info('Hello, Teacher!'))
+print(is_contains('TEACHER', ['master', 'chef', 'sir', 'lord']))
+print(string_info('Ragnarok'))
 
 print(calls)
